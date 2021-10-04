75fd4b47f6b6945cb1e142ec2fae506ad8926880
Semenov Artem 529 <a.s.semenov@student.khai.edu>
2021-10-04 12:58:14 +0300

    ChatBot 3-rd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -13,5 +13,15 @@ public class ChatBot {
         Scanner in = new Scanner(System.in);
         String name = in.next();
         System.out.println("What a great name you have," + name);
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of Dividing your age by 3,5 and 7.");
+        int remainder3 = in.nextInt();
+        int remainder5 = in.nextInt();
+        int remainder7 = in.nextInt();
+        int age = (remainder3*70 + remainder5*21 + remainder7*15)%105;
+        System.out.println("Your age is " + age + " ;that's a good time to start programming");
+
+
+
     }
 }
